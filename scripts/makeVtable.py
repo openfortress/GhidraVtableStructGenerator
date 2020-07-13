86da143a27a7d8dea965c6cf1af042ed30cb6194
Mangmasta <mangmasta@gmail.com>
2020-07-13 15:57:47 -0700

    Fixed function parameters not showing up.  Cleaned up some code.

--- scripts/makeVtable.py
+++ scripts/makeVtable.py
@@ -13,6 +13,7 @@ from ghidra.program.model.data import PointerDataType
 from ghidra.program.model.data import FunctionDefinitionDataType
 from ghidra.program.model.data import GenericCallingConvention
 from ghidra.program.model.data import ParameterDefinitionImpl
+from ghidra.program.model.data import CategoryPath
 
 dataManager = currentProgram.getDataTypeManager()
 
@@ -32,17 +33,6 @@ listing = currentProgram.getListing()
 addr = askAddress("Location of vtable", "Input offset where class vtable begins")
 structName = askString("Name of struct", "Input name of structure to be added")
 structData = StructureDataType(structName, 0)
-dt = PointerDataType()
-
-preexist = False
-lofd = dataManager.getAllStructures()
-for l in lofd:
-	if l.getName() == structName:
-		preexiststruct = l
-		preexist = True
-		break
-
-
 
 print(addr)
 codeUnits = listing.getCodeUnits(addr, True)
@@ -50,28 +40,22 @@ codeUnits = listing.getCodeUnits(addr, True)
 keepgoing = True
 while keepgoing:
 	fs = next(codeUnits)
-	#print("0x{} : {:16} {}".format(fs.getAddress(), hexlify(fs.getBytes()), fs.toString()))
 	valparts = fs.toString().split()
 	if valparts[0] == "addr":
 		print("YES, this is an pointer")
 		fntoadd = functionManager.getFunctionContaining(getAddress(valparts[1]))
 		print(fntoadd)
 		if fntoadd != None:
-			dt = FunctionDefinitionDataType(fntoadd, False) #Second parameter is "Formality", dont know exactly what it does
-			#dt.setGenericCallingConvention(GenericCallingConvention.get(3))
-			#fnparams = dt.getArguments()
-			#className = functionManager.getFunctionContaining(getAddress(valparts[1])).toString().split(":")[0]
-			#paramtoadd = ParameterDefinitionImpl("this", getDataTypeFromString(className), "") 
-			#fnparams.insert(0, paramtoadd)
-			#dt.setArguments(fnparams)
-			#ptr = PointerDataType(dt)
-			structData.add(dt, 4, functionManager.getFunctionContaining(getAddress(valparts[1])).toString(), "")
+			dt = FunctionDefinitionDataType(fntoadd, False) #Second parameter is "Formality", I think this strips the "this" parameter, so lets not set this True
+			dt.setCategoryPath(CategoryPath("/" + structName))
+			dtAdded = dataManager.addDataType(dt, DataTypeConflictHandler.REPLACE_HANDLER)
+			ptr = PointerDataType(dtAdded)
+			ptr.setCategoryPath(CategoryPath("/" + structName))
+			ptrAdded = dataManager.addDataType(ptr, DataTypeConflictHandler.REPLACE_HANDLER)
+			structData.add(ptrAdded, 4, functionManager.getFunctionContaining(getAddress(valparts[1])).toString(), "")
 	else:
 		keepgoing = False
 
-if preexist == True:
-	dataManager.replaceDataType(preexiststruct, structData, False)
-	print("Replaced " + structName)
-else:
-	dataManager.addDataType(structData, DataTypeConflictHandler.DEFAULT_HANDLER)
-	print("Created " + structName)
+
+dataManager.addDataType(structData, DataTypeConflictHandler.REPLACE_HANDLER)
+print("Created " + structName)
