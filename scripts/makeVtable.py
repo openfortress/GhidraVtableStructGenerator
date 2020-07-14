ae8569193c4d02db52c0352ffeae0216ce84e79c
Mangmasta <mangmasta@gmail.com>
2020-07-13 17:11:25 -0700

    Vtable name not needed to be input anymore.  Automatically adds the vtable struct to the class structure after finished generating

--- scripts/makeVtable.py
+++ scripts/makeVtable.py
@@ -23,39 +23,64 @@ def getAddress(offset):
 def getDataTypeFromString(dtName):
 	listOfDataTypes = dataManager.getAllDataTypes()
 	for dtI in listOfDataTypes:
-		if l.getName() == dtName:
-			return l
+		if dtI.getName() == dtName:
+			return dtI
+
+def getClassName(unparsedFnName):
+	tmpLst = unparsedFnName.split("::")
+	return tmpLst[0]
 
 functionManager = currentProgram.getFunctionManager()
 listing = currentProgram.getListing()
 
 
 addr = askAddress("Location of vtable", "Input offset where class vtable begins")
-structName = askString("Name of struct", "Input name of structure to be added")
-structData = StructureDataType(structName, 0)
+#structName = askString("Name of struct", "Input name of structure to be added")
+
 
 print(addr)
-codeUnits = listing.getCodeUnits(addr, True)
 
-keepgoing = True
-while keepgoing:
-	fs = next(codeUnits)
-	valparts = fs.toString().split()
-	if valparts[0] == "addr":
-		print("YES, this is an pointer")
-		fntoadd = functionManager.getFunctionContaining(getAddress(valparts[1]))
-		print(fntoadd)
-		if fntoadd != None:
-			dt = FunctionDefinitionDataType(fntoadd, False) #Second parameter is "Formality", I think this strips the "this" parameter, so lets not set this True
-			dt.setCategoryPath(CategoryPath("/" + structName))
-			dtAdded = dataManager.addDataType(dt, DataTypeConflictHandler.REPLACE_HANDLER)
-			ptr = PointerDataType(dtAdded)
-			ptr.setCategoryPath(CategoryPath("/" + structName))
-			ptrAdded = dataManager.addDataType(ptr, DataTypeConflictHandler.REPLACE_HANDLER)
-			structData.add(ptrAdded, 4, functionManager.getFunctionContaining(getAddress(valparts[1])).toString(), "")
-	else:
-		keepgoing = False
+def generateVtableStruct(vtableAddr):
+	codeUnits = listing.getCodeUnits(addr, True)
+	vtableClassName = ""
+	vtableName = ""
+	structData = None
+	keepgoing = True
+	while keepgoing:
+		fs = next(codeUnits)
+		valparts = fs.toString().split()
+		if valparts[0] == "addr":
+			#print("YES, this is an pointer")
+			fntoadd = functionManager.getFunctionContaining(getAddress(valparts[1]))
+			if vtableName == "":
+				vtableClassName = getClassName(fntoadd.toString())
+				vtableName = "vtable" + vtableClassName
+				structData = StructureDataType(vtableName, 0)
+				print("Making vtable for " + vtableClassName)
+			#print(fntoadd)
+			if fntoadd != None:
+				dt = FunctionDefinitionDataType(fntoadd, False) #Second parameter is "Formality", I think this strips the "this" parameter, so lets not set this True
+				dt.setCategoryPath(CategoryPath("/" + vtableName))
+				dtAdded = dataManager.addDataType(dt, DataTypeConflictHandler.REPLACE_HANDLER)
+				ptr = PointerDataType(dtAdded)
+				ptr.setCategoryPath(CategoryPath("/" + vtableName))
+				ptrAdded = dataManager.addDataType(ptr, DataTypeConflictHandler.REPLACE_HANDLER)
+				structData.add(ptrAdded, ptrAdded.getLength(), functionManager.getFunctionContaining(getAddress(valparts[1])).toString(), "")
+		else:
+			keepgoing = False
 
 
-dataManager.addDataType(structData, DataTypeConflictHandler.REPLACE_HANDLER)
-print("Created " + structName)
+	vtableCDataType = dataManager.addDataType(structData, DataTypeConflictHandler.REPLACE_HANDLER)
+	vtableCDataTypePtr = PointerDataType(vtableCDataType)
+	vtableDTtoAdd = dataManager.addDataType(vtableCDataTypePtr, DataTypeConflictHandler.REPLACE_HANDLER)
+	print("Created " + vtableName)
+
+	classDT = getDataTypeFromString(vtableClassName)
+	print("Adding vtable to " + classDT.getName() + " structure")
+	if classDT.getDataTypeAt(0) != None:
+		classDT.delete(0)
+	classDT.insert(0, vtableDTtoAdd, vtableDTtoAdd.getLength(), "vtable", "")
+	classDT.realign()
+
+
+generateVtableStruct(addr)
