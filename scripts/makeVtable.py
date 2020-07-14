f23f0d337b1d23922e44809b668365564a439566
Mangmasta <mangmasta@gmail.com>
2020-07-13 18:49:51 -0700

    Start of implementing iterating through all vtables

--- scripts/makeVtable.py
+++ scripts/makeVtable.py
@@ -10,12 +10,15 @@ from ghidra.program.model.data import DataTypeConflictHandler
 from ghidra.program.model.data import StructureDataType
 from ghidra.program.model.data import DataType
 from ghidra.program.model.data import PointerDataType
+from ghidra.program.model.data import Undefined4DataType
 from ghidra.program.model.data import FunctionDefinitionDataType
 from ghidra.program.model.data import GenericCallingConvention
 from ghidra.program.model.data import ParameterDefinitionImpl
 from ghidra.program.model.data import CategoryPath
 
 dataManager = currentProgram.getDataTypeManager()
+
+
 
 def getAddress(offset):
 	return currentProgram.getAddressFactory().getDefaultAddressSpace().getAddress(offset)
@@ -34,20 +37,21 @@ functionManager = currentProgram.getFunctionManager()
 listing = currentProgram.getListing()
 
 
-addr = askAddress("Location of vtable", "Input offset where class vtable begins")
+#addr = askAddress("Location of vtable", "Input offset where class vtable begins")
 #structName = askString("Name of struct", "Input name of structure to be added")
 
 
-print(addr)
+#print(addr)
 
 def generateVtableStruct(vtableAddr):
-	codeUnits = listing.getCodeUnits(addr, True)
+	codeUnits = listing.getCodeUnits(vtableAddr.add(8), True)
 	vtableClassName = ""
 	vtableName = ""
 	structData = None
 	keepgoing = True
-	while keepgoing:
-		fs = next(codeUnits)
+	#tmp = next(codeUnits)
+	#tmp = next(codeUnits)
+	for fs in codeUnits:
 		valparts = fs.toString().split()
 		if valparts[0] == "addr":
 			#print("YES, this is an pointer")
@@ -57,7 +61,8 @@ def generateVtableStruct(vtableAddr):
 				vtableName = "vtable" + vtableClassName
 				structData = StructureDataType(vtableName, 0)
 				print("Making vtable for " + vtableClassName)
-			#print(fntoadd)
+				monitor.setMessage("Observe: Making vtable for " + vtableClassName)
+			print(fntoadd)
 			if fntoadd != None:
 				dt = FunctionDefinitionDataType(fntoadd, False) #Second parameter is "Formality", I think this strips the "this" parameter, so lets not set this True
 				dt.setCategoryPath(CategoryPath("/" + vtableName))
@@ -67,7 +72,12 @@ def generateVtableStruct(vtableAddr):
 				ptrAdded = dataManager.addDataType(ptr, DataTypeConflictHandler.REPLACE_HANDLER)
 				structData.add(ptrAdded, ptrAdded.getLength(), functionManager.getFunctionContaining(getAddress(valparts[1])).toString(), "")
 		else:
-			keepgoing = False
+			break
+		
+			
+			
+
+			
 
 
 	vtableCDataType = dataManager.addDataType(structData, DataTypeConflictHandler.REPLACE_HANDLER)
@@ -80,7 +90,31 @@ def generateVtableStruct(vtableAddr):
 	if classDT.getDataTypeAt(0) != None:
 		classDT.delete(0)
 	classDT.insert(0, vtableDTtoAdd, vtableDTtoAdd.getLength(), "vtable", "")
-	classDT.realign()
+	#classDT.realign()
 
 
-generateVtableStruct(addr)
+##CODE FROM NOPEY
+#Finds all VTables, prints them out in the console
+#@author Magnus "Nopey" Larsen
+#@category 
+#@keybinding 
+#@menupath Tools.Misc.Find all VTables
+#@toolbar 
+
+symbol_table = currentProgram.getSymbolTable()
+
+symbols = symbol_table.getSymbolIterator()
+
+monitor.initialize(symbol_table.getNumSymbols())
+
+for symbol in symbols:
+    monitor.checkCanceled()
+    if symbol.getName().startswith("__ZTV"):
+        print(symbol)
+		vtA = symbol.getAddress()
+		generateVtableStruct(vtA)
+    monitor.incrementProgress(1)
+
+###END CODE FROM NOPEY
+
+#generateVtableStruct(addr)
